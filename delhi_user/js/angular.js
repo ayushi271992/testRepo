f52f6e3b9e17eea1b251d483976c7e12deec36c3
NishthaAggarwal <nishtha92aggarwal@gmail.com>
2016-06-29 22:12:02 +0530

    all 6 categories loading the details from db and showing in div part also

--- delhi_user/js/angular.js
+++ delhi_user/js/angular.js
@@ -8,17 +8,17 @@ app.controller("MyCtrl", ['$scope','$http',function($scope,$http)
   {
     $http.post('database/Heritage_Seeker_Leftpanel_Load.php').success(function(data)
     {
-      $scope.content2=data;
+      $scope.content_heritage=data;
       //$scope.msg="something something went wrong";
     });
   }; 
 
   $scope.Heritage_Seeker_openinfo=function(image)
   {  
-    $('#container1').css('display','block');
+    $('#container3').css('display','block');
     $http.post('database/Heritage_Seeker_Info_Load.php',{"Heritage_Seeker_Image_Name1":image.name}).success(function(data)
     {
-      $scope.content1=data;
+      $scope.content_heritage_info=data;
     });
   };
 
@@ -28,15 +28,15 @@ app.controller("MyCtrl", ['$scope','$http',function($scope,$http)
   {
     $http.post('database/Food_Fanatic_Leftpanel_Load.php').success(function(data)
     {
-      $scope.content2=data;
+      $scope.content_food=data;
     });
   }; 
 
   $scope.Food_Fanatic_openinfo=function(image)
-  {  
+  {   $('#container1').css('display','block');
     $http.post('database/Food_Fanatic_Info_Load.php',{"Food_Fanatic_Image_Name1":image.name}).success(function(data)
     {
-      $scope.content1=data;
+      $scope.content_food_info=data;
     });
   };
 
@@ -46,15 +46,15 @@ app.controller("MyCtrl", ['$scope','$http',function($scope,$http)
   {
     $http.post('database/Fun_Lovers_Leftpanel_Load.php').success(function(data)
     {
-      $scope.content2=data;
+      $scope.content_fun=data;
     });
   }; 
 
   $scope.Fun_Lovers_openinfo=function(image)
-  {  
+  {   $('#container2').css('display','block');
     $http.post('database/Fun_Lovers_Info_Load.php',{"Fun_Lovers_Image_Name1":image.name}).success(function(data)
     {
-      $scope.content1=data;
+      $scope.content_fun_info=data;
     });
   };
 
@@ -64,15 +64,15 @@ $scope.Music_Mania = function ()
   {
     $http.post('database/Music_Mania_Leftpanel_Load.php').success(function(data)
     {
-      $scope.content2=data;
+      $scope.content_music=data;
     });
   }; 
 
   $scope.Music_Mania_openinfo=function(image)
-  {  
+  {   $('#container4').css('display','block');
     $http.post('database/Music_Mania_Info_Load.php',{"Music_Mania_Image_Name1":image.name}).success(function(data)
     {
-      $scope.content1=data;
+      $scope.content_music_info=data;
     });
   };
 
@@ -82,15 +82,15 @@ $scope.Shopaholic = function ()
   {
     $http.post('database/Shopaholic_Leftpanel_Load.php').success(function(data)
     {
-      $scope.content2=data;
+      $scope.content_shopping=data;
     });
   }; 
 
   $scope.Shopaholic_openinfo=function(image)
-  {  
+  {   $('#container5').css('display','block');
     $http.post('database/Shopaholic_Info_Load.php',{"Shopaholic_Image_Name1":image.name}).success(function(data)
     {
-      $scope.content1=data;
+      $scope.content_shopping_info=data;
     });
   };
 
@@ -100,15 +100,15 @@ $scope.Spiritual_Divine = function ()
   {
     $http.post('database/Spiritual_Divine_Leftpanel_Load.php').success(function(data)
     {
-      $scope.content2=data;
+      $scope.content_spiritual=data;
     });
   }; 
 
   $scope.Spiritual_Divine_openinfo=function(image)
-  {  
+  {   $('#container6').css('display','block');
     $http.post('database/Spiritual_Divine_Info_Load.php',{"Spiritual_Divine_Image_Name1":image.name}).success(function(data)
     {
-      $scope.content1=data;
+      $scope.content_spiritual_info=data;
     });
   };
 
